3d526cdbb37d32fa163eaa5f9eddfd0e493531db
jjesusteran-mori <iokoumorit@lgmail.com>
2020-12-10 14:52:14 -0800

    added fuctions to check user input to match website criteria

--- js/script.js
+++ js/script.js
@@ -23,9 +23,13 @@ function showSuccess(input) {
 }
 
 // check email is valid 
-function isValidEmail(email){
+function checkEmail(input){
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());    
+    if(re.test(input.value.trim())){
+        showSuccess(input);
+    }else{
+        showError(input, "Email is not valid")
+    };
 }
 
 // check required fields
@@ -41,6 +45,24 @@ function checkRequired(inputArr) {
     });
 }
 
+// check input length
+function checkLength(input, min, max) {
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} must be at least ${min}`)
+    }else if(input.value.length > max){
+        showError(input, `${getFieldName(input)} must be less than ${max}`)
+    }else{
+        showSuccess(input)
+    }
+}
+
+// check passwords match
+function checkPasswordsMatch(input1, input2){
+    if(input1.value !== input2.value){
+        showError(input, "Passwords do not match");
+    }
+}
+
 // Get fieldname
 function getFieldName(input) {
     // getting first letter of input.id and uppercase then adding the rest of the input.id with the first char(0)
@@ -51,5 +73,9 @@ function getFieldName(input) {
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    checkRequired([username, email, password, password2])
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2)
 })
