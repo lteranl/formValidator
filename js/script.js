795f8c88548c044e6a096951f5f5c042eabc7c2d
jjesusteran-mori <iokoumorit@lgmail.com>
2020-12-10 14:56:05 -0800

    fixed minor bug

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ function checkLength(input, min, max) {
 // check passwords match
 function checkPasswordsMatch(input1, input2){
     if(input1.value !== input2.value){
-        showError(input, "Passwords do not match");
+        showError(input2, "Passwords do not match");
     }
 }
 
